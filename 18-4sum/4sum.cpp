e72890c5c486cbfd796471121f78f7b0cd6575ae
DEEPARNA SARKAR <sarkardeeparna0@gmail.com>
2026-08-04 22:35:36 +0530

    Time: 13 ms (94.05%) | Memory: 17.4 MB (85.33%) - LeetSync

--- 18-4sum/4sum.cpp
+++ 18-4sum/4sum.cpp
@@ -5,13 +5,13 @@ public:
         int n = nums.size();
         vector<vector<int>> ans;
 
-        for (int i = 0; i < n - 3; i++) {
+        for (int i = 0; i < n ; i++) {
 
             // Skip duplicate values for i
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
 
-            for (int j = i + 1; j < n - 2; j++) {
+            for (int j = i + 1; j < n; j++) {
 
                 // Skip duplicate values for j
                 if (j > i + 1 && nums[j] == nums[j - 1])
